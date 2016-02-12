017c0fdc546b7f1ca1e2c2bc2eb217662d2fb679
petrucior <petrucior@gmail.com>
2016-02-12 04:18:37 -0300

    Arquivo gerenciador de múltiplas fóveas
    Este arquivo será conterá os protótipos das funções que controlam
    as múltiplas fóveas que poderão ser instanciadas.

---
+++ multiFoveation.h
@@ -0,0 +1,57 @@
+/**
+ * \file multiFoveation.h
+ *
+ * \brief This file contains the prototype of structure multifoveated.
+ *
+ * \author 
+ * Petrucio Ricardo Tavares de Medeiros \n
+ * Universidade Federal do Rio Grande do Norte \n
+ * Departamento de Computacao e Automacao Industrial \n
+ * petrucior at gmail (dot) com
+ *
+ * \version 0.1
+ * \date February 2016
+ *
+ * \copyright
+ * Copyright (C) 2014, Petrúcio Ricardo <petrucior@gmail.com>
+ * If you use this software for academic purposes, consider citing the related
+ * paper: Rafael Beserra Gomes, Bruno Motta de Carvalho, Luiz Marcos Garcia 
+ * Gonçalves, Visual attention guided features selection with foveated images,
+ * Neurocomputing, Volume 120, 23 November 2013, Pages 34-44, ISSN 0925-2312,
+ * http://dx.doi.org/10.1016/j.neucom.2012.10.033.
+ *
+ * This file is part of foveatedFeatures software.
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later 
+ * version. This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef MULTIFOVEATION_H
+#define MULTIFOVEATION_H
+
+/**
+ * \struct MultiFoveation 
+ *
+ * \brief Struct for múltiples foveae.
+ */
+struct MultiFoveation{
+
+  /**
+   * \fn void MultiFoveation(int foveae)
+   *
+   * \brief Constructor multi foveae.
+   *
+   * \param foveae - Number of foveae
+   */
+  void MultiFoveation(int foveas);
+  
+  
+
+};
+
+#endif
