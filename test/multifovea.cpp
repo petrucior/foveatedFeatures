1cdb27eea5a4277cac74c6ce425ce839e7790431
petrucior <petrucior@gmail.com>
2016-02-25 22:10:51 -0300

    Modificando as cores das estruturas foveadas
    Nesta atualização do código utilizo em cada fóvea uma cor específica
    além da adição de uma legenda à imagem de saída. A fóvea 0 constroi
    todo o espaço de multiresolução, enquanto que a inserção das próximas
    fóveas elimina o processamento das interseções.

--- test/multifovea.cpp
+++ test/multifovea.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include "../multiFoveation.h"
 #include "opencv2/core/core.hpp"
 #include "opencv2/features2d/features2d.hpp"
@@ -22,20 +25,7 @@ static void on_mouse(int event, int x, int y, int flags, void *_param){
 
 
 int main(int argc, char** argv){
-  //MultiFoveation m(2);
-  //m.intersection(0, 2);
-    // Mat image = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
-    // if ( image.empty() ) return -1;
-    // std::vector<String> s;
-    // s.push_back(argv[2]);
-    // s.push_back(argv[3]);
-    // MultiFoveation foveas(2, image, s);
-  //FoveatedHessianDetectorParams p = foveas.getParams(0);
-  //std::cout << p.nOctaveLayers << std::endl;
-  //std::cout << p.foveaModel.fx << std::endl;
-  //Point pointIntersection = foveas.intersection(1, 1, image.size(), 0, 1);
-  //std::cout << "Intersection is realized in: (" << pointIntersection.x << ", " << pointIntersection.y << ")" << std::endl;
-
+  
   if ( argc < 3 ){
     helpExtract();
     return -1;
@@ -54,6 +44,16 @@ int main(int argc, char** argv){
   MultiFoveation foveas(argc-2, image, s);
   std::cout << argc-2 << " Fóveas criadas" << std::endl;
   namedWindow("keypoints", 1);
+
+  std::vector<Scalar> colors;
+  srand (time(NULL)); // Initialize random seed
+  for (int i = 0; i < argc-2; i++){
+    int r = rand() % 256; // 0 - 255
+    int g = rand() % 256; // 0 - 255
+    int b = rand() % 256; // 0 - 255
+    colors.push_back(Scalar(b, g, r));
+  }
+    
   
   while(true){
     vector<KeyPoint> keypointSave;
@@ -75,7 +75,7 @@ int main(int argc, char** argv){
     Mat outputImg;
     drawKeypoints(image, keypointSave, outputImg, Scalar::all(-1), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
     for (int i = 0; i < argc-2; i++)
-      (i == 0) ? drawMultiFoveatedLevels(outputImg, foveas.getParams(i), Scalar(255, 255, 255)) : drawMultiFoveatedLevels(outputImg, foveas.getParams(i), Scalar(0, 0, 255));
+      drawMultiFoveatedLevels(outputImg, foveas.getParams(i), i, colors[i]);
     
     imshow("keypoints", outputImg);
     
